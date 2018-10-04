3868fcacba007eae9c45da2722669cad53d9d04f
IvanKorotchenko <korotivan2005@yandex.ru>
2018-10-05 00:06:40 +0300

    modified

--- Pythonfile/bot.py
+++ Pythonfile/bot.py
@@ -3,7 +3,7 @@ import bs4
 from datetime import date
 site = "https://sinoptik.com.ru/погода-москва"
 html_list = ['']
-temp = ['']
+temp = []
 day_weekday = 0
 
 def connect_site(site):
@@ -13,22 +13,10 @@ def connect_site(site):
 
 def parse_temp():
     global temp
-    p1_temp=html_list.select('.temperature .p1')
-    p2_temp=html_list.select('.temperature .p2')
-    p3_temp=html_list.select('.temperature .p3')
-    p4_temp=html_list.select('.temperature .p4')
-    p5_temp=html_list.select('.temperature .p5')
-    p6_temp=html_list.select('.temperature .p6')
-    p7_temp=html_list.select('.temperature .p7')
-    p8_temp=html_list.select('.temperature .p8')
-    temp[0] = p1_temp[0].getText()
-    temp.append(p2_temp[0].getText())
-    temp.append(p3_temp[0].getText())
-    temp.append(p4_temp[0].getText())
-    temp.append(p5_temp[0].getText())
-    temp.append(p6_temp[0].getText())
-    temp.append(p7_temp[0].getText())
-    temp.append(p8_temp[0].getText())
+    for i in range(1, 9):
+        temp.append(html_list.select('.temperature .p'+str(i))[0].getText())
+
+                    
 def get_weekday():
     global day_weekday
     day_weekday = date.today().weekday()
@@ -50,21 +38,24 @@ def get_weekday():
 
 
 if __name__ == '__main__':
+    #функция для получения данных сайта 
     connect_site(site)
+    #функция для обработки данных сайта 
     parse_temp()
+     #функция для вывода дня недели 
     print(get_weekday())
-    for x in range(5):
-        if x == 1:
-            print(temp[0] + " Ночью (0:00)")
-            print(temp[1] + " Ночью (3:00)")
-        if x == 2:
-            print(temp[2] + " Утром (6:00)")
-            print(temp[3] + " Утром (9:00)")
-        if x == 3:
-            print(temp[4] + " Днем (12:00)")
-            print(temp[5] + " Днем (15:00)")
-        if x == 4:
-            print(temp[6] + " Вечером (18:00)")
-            print(temp[7] + " Вечером (21:00)")
+    #Ночь
+    print(temp[0] + " Ночью (0:00)")
+    print(temp[1] + " Ночью (3:00)")
+    #Утро  
+    print(temp[2] + " Утром (6:00)")
+    print(temp[3] + " Утром (9:00)")
+    #День
+    print(temp[4] + " Днем (12:00)")
+    print(temp[5] + " Днем (15:00)")
+    #Вечер
+    print(temp[6] + " Вечером (18:00)")
+    print(temp[7] + " Вечером (21:00)")
+    #нформация о данных
     print("Данные взяты с сайта sinoptik.ru по городу Москва")
     input("Что бы закрыть программу, нажмите ENTER")
