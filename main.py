0b96df5f70530c5227f4fc641d2c23e084f98566
Ivan <korotivan2005@yandex.ru>
2024-10-21 13:07:09 +0300

    Example app on DearPyGUI

---
+++ main.py
@@ -0,0 +1,16 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+with dpg.window(tag="Primary Window"):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Save")
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.create_viewport(title='Custom Title', width=600, height=200)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
